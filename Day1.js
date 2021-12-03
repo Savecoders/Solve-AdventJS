0443f6d5519a2ff8f200c879071c5b69f175e12f
Savecoders <pablopincay2003@gmail.com>
2021-12-02 23:50:45 -0500

    add two days of AdventJS by midudev

---
+++ Day1.js
@@ -0,0 +1,28 @@
+/*
+  Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color. 
+  Haz una función que devuelva una lista con todas las ovejas que sean de color
+  rojo y que además su nombre contenga tanto las letras n Y a, sin importar 
+  el orden, las mayúsculas o espacios.
+  !Por ejemplo, si tenemos las ovejas:
+  const ovejas = [
+    { name: 'Noa', color: 'azul' },
+    { name: 'Euge', color: 'rojo' },
+    { name: 'Navidad', color: 'rojo' },
+    { name: 'Ki Na Ma', color: 'rojo'}
+  ]
+  !Al ejecutar el método debería devolver lo siguiente:
+  const ovejasFiltradas = contarOvejas(ovejas)
+
+  console.log(ovejasFiltradas)
+  
+  [{ name: 'Navidad', color: 'rojo' },{ name: 'Ki Na Ma', color: 'rojo' }]
+*/
+const contarOvejas = (ovejas) =>
+	ovejas.filter(
+		({ name, color }) =>
+			color === 'rojo' &&
+			name.toLocaleLowerCase().includes('a') &&
+			name.toLocaleLowerCase().includes('n')
+	);
+
+export default contarOvejas;
---
+++ Day2.js
@@ -0,0 +1,33 @@
+/* 
+Te ha llegado una carta ✉️ con todos los regalos que debes preparar. 
+El tema es que es una cadena de texto y es muy difícil de leer 😱. 
+¡Menos mal que han puesto cada regalo separado por espacio! 
+(aunque ten cuidado, porque al ser niños, igual han colado más espacios de la cuenta)
+
+Encima nos hemos dado cuenta que algunas palabras vienen con un _ delante 
+de la palabra, por ejemplo _playstation, que significa que está tachado y 
+no se tiene que contar.
+
+!Transforma el texto a un objeto que contenga el nombre de cada regalo 
+!y las veces que aparece. Por ejemplo, si tenemos el texto:
+
+* const carta = 'bici coche balón _playstation bici coche peluche'
+
+!Al ejecutar el método debería devolver lo siguiente:
+
+* const regalos = listGifts(carta)
+* console.log(regalos)
+* {bici: 2,coche: 2,balón: 1,peluche: 1}
+*/
+const listGifts = (letter) => {
+	const ArrLetters = letter.trim().split(' ');
+	const ArrFilterGifts = ArrLetters.filter((letter) => !letter.includes('_'));
+	const objGifts = {};
+	ArrFilterGifts.forEach((gift) => {
+		objGifts[gift] = !objGifts[gift] ? 1 : objGifts[gift] + 1;
+	});
+	return objGifts;
+};
+export default listGifts;
+const regalos = listGifts('bici coche balón _playstation bici coche peluche');
+console.log(regalos);
