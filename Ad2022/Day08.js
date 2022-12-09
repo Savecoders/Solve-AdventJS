9020e4c241b240b9ebcf6ce12a75b7b0337ca6aa
Savecoders <pablopincay2003@gmail.com>
2022-12-09 03:51:10 +0000

    add Day8 AdventJS 2022

---
+++ Ad2022/Day08.js
@@ -0,0 +1,12 @@
+function checkPart(part) {
+  const reverseArr = (str) => [...str].reverse().join('')
+  
+  // Check if the string is already a palindrome
+  if (reverseArr(part) === part) return true
+
+  // Check if removing one character makes the string a palindrome
+  return [...part].some((_, index) => {
+    const slicePart = part.substring(0, index) + part.substring(index + 1);
+    return slicePart === reverseArr(slicePart)
+  })
+}
