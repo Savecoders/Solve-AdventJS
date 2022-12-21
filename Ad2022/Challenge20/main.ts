a3cf4d854b256fc9ca4d8d4f1015ffd4e1334675
Savecoders <pablopincay2003@gmail.com>
2022-12-20 22:46:53 -0500

    add Day20 Adventjs 2022

---
+++ Ad2022/Challenge20/main.ts
@@ -0,0 +1,47 @@
+interface ReindeerType {
+  type: string;
+  weightCapacity: number;
+}
+
+interface Gift {
+  country: string;
+  weight: number;
+}
+
+interface Reindeer {
+  type: string;
+  num: number;
+}
+
+function howManyReindeers(
+  reindeerTypes: ReindeerType[],
+  gifts: Gift[],
+): {
+  country: string;
+  reindeers: Reindeer[];
+}[] {
+  return gifts.map(({ weight, country }) => {
+    const filterReindeer = reindeerTypes
+      .filter(({ weightCapacity }) => weightCapacity < weight)
+      .sort((a, b) => b.weightCapacity - a.weightCapacity);
+
+    let sumCapacity: number = filterReindeer.reduce(
+      (acc, { weightCapacity }) => acc + weightCapacity,
+      0,
+    );
+
+    const reindeers: Reindeer[] = filterReindeer
+      .map(
+        ({ type, weightCapacity }) => {
+          const num = Math.floor(weight / sumCapacity);
+          weight -= num * weightCapacity;
+          sumCapacity -= weightCapacity;
+          return { type, num };
+        },
+      );
+
+    return { country, reindeers };
+  });
+}
+
+export { howManyReindeers };
